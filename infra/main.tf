4fd4db9f633d0c6bc9cde8bbc961df732691e8b0
Julian Dahl <juliandahl99@gmail.com>
2024-11-21 12:38:29 +0000

    CloudWatch alarm

--- infra/main.tf
+++ infra/main.tf
@@ -40,7 +40,7 @@ resource "aws_iam_role" "lambda_role" {
   })
 }
 
-# IAM Policy for Lambda (Updated with CloudWatch Permissions)
+# IAM Policy for Lambda
 resource "aws_iam_policy" "lambda_policy" {
   name        = "lambda_sqs_policy_56"
   description = "Policy for Lambda to access SQS, S3, CloudWatch Logs, and Bedrock"
@@ -82,7 +82,7 @@ resource "aws_iam_role_policy_attachment" "lambda_attachment" {
   policy_arn = aws_iam_policy.lambda_policy.arn
 }
 
-# Lambda Function (Updated with Candidate Number in Name)
+# Lambda Function
 resource "aws_lambda_function" "image_lambda" {
   filename         = "${path.module}/lambda_sqs.zip"
   function_name    = "lambda_sqs_image_generator_56"
@@ -107,4 +107,40 @@ resource "aws_lambda_event_source_mapping" "sqs_trigger" {
   function_name    = aws_lambda_function.image_lambda.arn
   enabled          = true
   batch_size       = 5
+}
+
+# CloudWatch Alarm for SQS
+resource "aws_cloudwatch_metric_alarm" "sqs_age_alarm" {
+  alarm_name          = "sqs-age-of-oldest-message-alarm-56"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "ApproximateAgeOfOldestMessage"
+  namespace           = "AWS/SQS"
+  period              = 60
+  statistic           = "Maximum"
+  threshold           = 60 # Set threshold in seconds
+  alarm_description   = "Triggered when the age of the oldest message in the SQS queue exceeds the threshold."
+  dimensions = {
+    QueueName = aws_sqs_queue.image_queue.name
+  }
+  actions_enabled = true
+  alarm_actions   = [aws_sns_topic.sqs_alarm_topic.arn]
+}
+
+# SNS Topic for Alarm
+resource "aws_sns_topic" "sqs_alarm_topic" {
+  name = "sqs-age-alarm-topic-56"
+}
+
+# SNS Subscription for Email Notifications
+resource "aws_sns_topic_subscription" "sqs_alarm_email_subscription" {
+  topic_arn = aws_sns_topic.sqs_alarm_topic.arn
+  protocol  = "email"
+  endpoint  = var.alert_email
+}
+
+# Alert Email Variable
+variable "alert_email" {
+  description = "Email address for CloudWatch alarm notifications"
+  type        = string
 }
